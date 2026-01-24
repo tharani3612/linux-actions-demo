c0bddcf42a0258054090f629ea5db50b0df99944
tharani3612 <tharanidharanm077@gmail.com>
2026-01-23 21:27:40 -0500

    update python application output

--- app.py
+++ app.py
@@ -2,5 +2,4 @@ print("WELCOME TO MY PAGE")
 print("My first GitHub Actions from Linux terminal")
 print("update by 22/01/26 sucessfully")
 print("exit")
-print("last seen saturday")O
-
+print("last seen saturday")
